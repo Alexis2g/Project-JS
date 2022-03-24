5da7639ec2cd68ceb02997c431c6b0bd903b37f5
Alexis2g <vuaille.alexis@gmail.com>
2022-03-24 22:01:32 +0100

    test netlify

--- Test.js
+++ Test.js
@@ -1,1 +1,15 @@
-console.log('salut')
+let http = require('http')
+
+let serveur = http.createServer()
+
+serveur.on('request', (request, reponse) => {
+
+    reponse.writeHead(200, {
+        'Content-type': 'text/html; charset=utf-8'
+    })
+    
+    reponse.end('salut ça va ?') 
+
+})
+
+serveur.listen(8080)
